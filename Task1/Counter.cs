f3f65d0169317df878273778b3b921eca5269612
WhiteMorro1177 <jackforest228@gmail.com>
2023-10-26 13:15:22 +0300

    beta-v0.1

---
+++ Task1/Counter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Task1
+{
+	internal class Counter
+	{
+		private List<long[]> _vector;
+		private int _num_threads;
+
+		private long vector_sum = 0;
+		private long vector_items_amount = 0;
+		private List<Thread> threads = new List<Thread>();
+
+		public int TimeSpent { get; private set; }
+
+		public Counter() { }
+		public Counter(long[] vector, int num_threads)
+		{
+			_vector = PrepareVector(vector, num_threads);
+			_num_threads = num_threads;
+			vector_items_amount = vector.Length;
+		}
+
+
+		public void Run()
+		{
+			var code_start = DateTime.Now;
+			Console.WriteLine("Prepare vector...");
+
+			Console.WriteLine(
+				"Time spent on prepare: {0} milliseconds",
+				(DateTime.Now - code_start).TotalMilliseconds
+			);
+
+
+			code_start = DateTime.Now; // restart timer
+			Console.WriteLine("Execute main code...");
+
+			foreach (var item in _vector)
+			{
+				var args = new object[] { item, _vector.IndexOf(item) };
+
+				Thread t = new Thread(SumVector);
+				threads.Add(t);
+				t.Start(args);
+			}
+
+			foreach (Thread t in threads) t.Join();
+
+			Console.WriteLine(
+				"Mean vector sum = {0}, time spent: {1} milliseconds",
+				vector_sum / vector_items_amount,
+				(DateTime.Now - code_start).TotalMilliseconds
+			);
+		}
+
+		private List<long[]> PrepareVector(long[] vector, int num_threads)
+		{
+			int item_amount_in_part = vector.Length / num_threads;
+
+			var splitted_vector = new List<long[]>();
+
+			for (int i = 0; i < num_threads; i++)
+				splitted_vector.Add(vector.Skip(i * item_amount_in_part).Take(item_amount_in_part).ToArray());
+			
+
+			return splitted_vector;
+		}
+
+		private void SumVector(object args)
+		{
+			var _args = args as object[];
+			long[] vector = _args[0] as long[];
+			int id = (int) _args[1];
+
+			foreach (long item in vector)
+			{
+				vector_sum += item;
+			}
+
+            Console.WriteLine("Thread {0} complete", id);
+        }
+	}	
+}
---
+++ Task1/MainClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+	internal class MainClass
+	{
+		static void Main(string[] args)
+		{
+			// initialize vector
+			long N = 100_000_000;
+			long[] vector = new long[N];
+			var r = new Random();
+
+			Console.WriteLine("Filling vector...");
+			for (int i = 0; i < vector.Length; i++)
+			{
+				vector[i] = i;
+			}
+
+			Console.WriteLine($"Vector contain {N} items\n\nPrepare to executing...");
+
+
+			// sequencial run
+			DoSequencially(vector);
+
+			// parallel run
+			DoParallel(vector, num_threads: 2);
+		}
+
+		private static void DoParallel(long[] vector, int num_threads)
+		{
+            Console.WriteLine("Start parallel programm\n");
+
+			var counter = new Counter(vector, num_threads);
+
+			counter.Run();
+        }
+
+		private static void DoSequencially(long[] vector)
+		{
+            Console.WriteLine("Start sequencial programm\n");
+            var code_start = DateTime.Now;
+
+			long vector_sum = 0;
+			for (int i = 0; i < vector.Length; i++)
+			{
+				vector_sum += vector[i];
+			}
+
+			Console.WriteLine(
+				"Mean vector sum = {0}, time spent: {1} milliseconds", 
+				vector_sum / vector.Length, 
+				(DateTime.Now - code_start).TotalMilliseconds
+			);
+		}
+	}
+}
